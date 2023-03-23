316ad3e033b0e402147c34956965754355928308
Женя Сладков <sladkovevgenii@gmail.com>
2023-03-23 13:31:29 +0200

    chnges1

--- Week10/MiniProject/btre/btre/settings.py
+++ Week10/MiniProject/btre/btre/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +58,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'btre.urls'
-import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
