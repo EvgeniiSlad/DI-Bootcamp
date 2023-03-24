7b69ab0077a0e167ce4c4570cc1d4d282bd37360
Женя Сладков <sladkovevgenii@gmail.com>
2023-03-24 18:28:08 +0300

    fixs

--- Week10/MiniProject/btre/contacts/admin.py
+++ Week10/MiniProject/btre/contacts/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
 from django.apps import apps
+from .models import Contact
 # Register your models here.
 
-models = apps.get_models()
-for model in models:
-    try:
-        admin.site. register (model)
-    except admin. sites.AlreadyRegistered:
-        pass
+class ContactAdmin(admin.ModelAdmin):
+   list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+   list_display_links = ('id', 'name')
+   search_fields = ('name', 'email', 'listing')
+   list_per_page = 25
+
+admin.site.register(Contact, ContactAdmin)
+
+
+#TODO:TO REGISTER ALL APPS FIXME:
+# models = apps.get_models()
+# for model in models:
+#     try:
+#         admin.site. register (model)
+#     except admin. sites.AlreadyRegistered:
+#         pass
 
 
 
